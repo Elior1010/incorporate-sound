7551adafc6dda98d0432022b7f861234fe73d573
Elior1010 <71310095+Elior1010@users.noreply.github.com>
2024-03-29 17:15:54 -0700

    Delete script.cs

--- script.cs
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    // Automatically play background sound
-    const backgroundAudio = document.getElementById('background-audio');
-    backgroundAudio.play().catch(error => {
-        console.error("Audio autoplay failed:", error);
-    });
-});
